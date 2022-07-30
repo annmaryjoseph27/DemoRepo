041b691e5981aa112a4517c1e768363a03a5f250
Ann Joseph <annmaryjoseph27@gmail.com>
2022-07-30 16:17:38 -0400

    added webelement in SignUp page

--- src/main/java/com/crm/qa/pages/SignUpPage.java
+++ src/main/java/com/crm/qa/pages/SignUpPage.java
@@ -1,7 +1,13 @@
 package com.crm.qa.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 import com.crm.qa.base.TestBase;
 
 public class SignUpPage extends TestBase{
+	
+	@FindBy(xpath = "//span[contains(text(),'Ann Joseph')]")
+	WebElement userNameLabel;
 
 }
